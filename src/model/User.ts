8965a2bc7f82431546d917fab0d8b8b0a6c03023
Krishna <gupta.krishnalko111@gmail.com>
2024-09-11 21:58:18 +0530

    Started this Again....

---
+++ src/model/User.ts
@@ -0,0 +1,81 @@
+import mongoose, {Schema, Document, Mongoose} from 'mongoose';
+
+
+// Url Interface
+ 
+export interface Url extends Document{
+    shortId: string,
+    redirectUrl: string,
+    createdAt: Date,
+}
+
+
+// Url Schema
+
+const UrlSchema: Schema<Url> = new Schema({
+shortId:{
+    type: String,
+    required: true,
+},
+redirectUrl:{
+    type: String,
+    required: true,
+},
+createdAt:{
+    type: Date,
+    required: true,
+    default: Date.now
+},
+})
+
+
+// User Interface
+
+export interface User extends Document{
+    username: string,
+    email: string,
+    password: string,
+    createdAt: Date,
+    urls: Url[],
+    passwordResetToken: string,
+    passwordResetTokenExpiry: Date,
+}
+
+
+// User Schema
+
+const UserSchema:Schema<User> = new Schema({
+    username:{
+        type: String,
+        required: true
+    },
+    email:{
+        type: String,
+        required: true,
+        unique: true,
+    },
+    password:{
+        type: String,
+        required: true
+    },
+    createdAt:{
+        type: Date,
+        required: true,
+        default: Date.now
+    },
+    urls: [UrlSchema],
+    passwordResetToken:{
+        type: String,
+    },
+    passwordResetTokenExpiry:{
+        type: Date,
+    },
+
+})
+
+
+const UserModel = (mongoose.models.User as mongoose.Model<User>) || mongoose.model<User>("User", UserSchema);
+
+const UelModel = (mongoose.models.Url as mongoose.Model<Url>) || mongoose.model<Url>("Url", UrlSchema);
+
+export {UserModel, UrlSchema}
